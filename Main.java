cc46bf2939d41e8645d48ecc3dabe245fb93e5e7
oguzhanarag <oguzhanarag@gmail.com>
2025-12-14 04:28:29 +0300

    1-Completed: commodityProfitInRange logic 2-Completed: bestDatOfMonth logic

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     private static int indexOfCommodity(String name) {
         for (int i = 0; i < COMMS; i++) {
-            if (commodities[i].equals(name)) return i;//parametre olarak gelen isimle aynı olup olmadığını kontrol eder
+            if (commodities[i].equals(name)) return i;
         }
         return -1;
     }
@@ -34,6 +34,10 @@ public class Main {
 
     // ======== 10 REQUIRED METHODS (Students fill these) ========
 
+
+    /* 1.Method
+    Bir ay içindeki en değerli comm
+     */
     public static String mostProfitableCommodityInMonth(int month) {
         if (month < 1 || month > MONTHS) return "Invalid month";//ocaktan önce ay yok yani birşey returnleyemeyiz
         int m = month - 1;//arrayler 0 dan başladığı için mesela kullanıcı ocaak ayını isterse 0.indexe denk gelir bu yüzden 1 çıkarmak laızm
@@ -43,10 +47,11 @@ public class Main {
         }
         int best = 0;
         for (int c = 1; c < COMMS; c++) if (totals[c] > totals[best]) best = c;
-        return commodities[best];//eğer herhangi bir total diğerinden daha karlı ise her seferinde onu en iyi olarak atiyom
+        return commodities[best]; //eğer herhangi bir total diğerinden daha karlı ise her seferinde onu en iyi olarak atiyom
     }
-
-   // bir günün toplam karı
+    /* 2.Method
+       Bir günün toplam karı
+     */
     public static int totalProfitOnDay(int month, int day) {
         if (month < 1 || month > MONTHS || day < 1 || day > DAYS) return -1;//geçersiz giriş
         int m = month - 1;//arrayler 0 dan başladığı için -1 koymamız lazım
@@ -55,13 +60,44 @@ public class Main {
         for (int c = 0; c < COMMS; c++) sum += profits[m][d][c]; //
         return sum;
     }
-
+    /* 3.Method
+    Bir commun belirli aralıkta toplam karını hesaplar(son günden girilen ilk güne kadar olan total kar)
+     */
     public static int commodityProfitInRange(String commodity, int from, int to) {
-        return 1234;
+        int index = indexOfCommodity(commodity);//bu comm kaçıncı indexde onu buluyo
+        if (index == -1 || from < 1 || to < 1 || from > to) return -1;//geçersiz değer
+        int sum = 0;
+        //tüm yılı geziyor nested loop yaparak ocak ayı 1-28 yaparken ilk loopda m 1 olunca şubat ayında 1-28 yapıyor
+        for (int m = 0; m < MONTHS; m++) {
+            for (int d = 0; d < DAYS; d++) {
+                int dayNumber = m * DAYS + d + 1;//burası önemli ay ve gün arasındaki ilişkiyi kuruyo ve ocak 28 çekerken şubata geçince 29. günden devam ediyor
+                if (dayNumber >= from && dayNumber <= to) { //eğer yukarıdaki aralığın içindeyse karı sum a ekliyor
+                    sum += profits[m][d][index];
+                }
+            }
+        }
+        return sum;
     }
-
+    /*
+    4.Method
+     Ayın en karlı günü bulma
+     */
     public static int bestDayOfMonth(int month) {
-        return 1234;
+        if (month < 1 || month > MONTHS) return -1; //ocaktan önce ay yok geçersiz
+        int m = month - 1;
+
+        long best = Long.MIN_VALUE; // bu javanın kendi kütüphanesinden bir sabit kendinden daha küçük bir değer görürse otomatik güncellenir
+        int bestDay = 1;
+
+        for (int d = 0; d < DAYS; d++) {
+            long sum = 0;
+            for (int c = 0; c < COMMS; c++) sum += profits[m][d][c];
+            if (sum > best) {
+                best = sum;
+                bestDay = d + 1;// d bir array indexi o yüzden 0 dan başlar fakat normal bi gün 0 olamicağı için +1 koyuyoz
+            }
+        }
+        return bestDay;
     }
 
     public static String bestMonthForCommodity(String comm) {
